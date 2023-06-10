23b3843a72c5272ca14b14155e6348f9228e24da
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-06-10 13:42:52 -0700

    v0.2.3-alpha.8

--- bindings/ruby/lib/version.rb
+++ bindings/ruby/lib/version.rb
@@ -1,4 +1,4 @@
 # automatically generated, do not edit by hand.
 module SqliteRegex
-  VERSION = "0.2.3-alpha.7"
+  VERSION = "0.2.3-alpha.8"
 end
--- python/datasette_sqlite_regex/datasette_sqlite_regex/version.py
+++ python/datasette_sqlite_regex/datasette_sqlite_regex/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.2.3-alpha.7"
+__version__ = "0.2.3-alpha.8"
 __version_info__ = tuple(__version__.split("."))
