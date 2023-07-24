8bfcac5141680153ba8a0eacb0c8029968ca730e
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-07-23 23:09:36 -0700

    v0.2.3

--- bindings/ruby/lib/version.rb
+++ bindings/ruby/lib/version.rb
@@ -1,4 +1,4 @@
 # automatically generated, do not edit by hand.
 module SqliteRegex
-  VERSION = "0.2.3-alpha.12"
+  VERSION = "0.2.3"
 end
--- bindings/sqlite-utils/sqlite_utils_sqlite_regex/version.py
+++ bindings/sqlite-utils/sqlite_utils_sqlite_regex/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.2.3-alpha.12"
+__version__ = "0.2.3"
 __version_info__ = tuple(__version__.split("."))
